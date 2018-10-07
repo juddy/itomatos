0a1342967ba566a5046bc16d40417a144414620e
William Kennedy <wjk@debian>
2018-10-06 23:53:14 -0700

    disable source from lb config and disable lb source command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-lb config --apt apt -a amd64 -b iso --bootloaders syslinux --debconf-frontend dialog --debian-installer true -d buster --mode debian --system normal --onie true --source true -s iso
+lb config --apt apt -a amd64 -b iso --bootloaders syslinux --debconf-frontend dialog --debian-installer true -d buster --mode debian --system normal --onie true -s iso
 
 lb bootstrap
 
@@ -6,6 +6,6 @@ sudo lb build
 
 sudo lb chroot
 
-sudo lb source
+#sudo lb source
 
 sudo ./binary.sh~
